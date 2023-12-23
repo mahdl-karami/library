f65d562eb7d2943bfb419d18bcd946fa66271f94
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2023-12-23 10:51:23 +0330

    header
    create header and styled.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <h1>Book App Starting</h1>
+    <>
+      <Header />
+    </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+const Head = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.7rem 1.2rem;
+  background: #2e2e2e;
+  border-radius: 15px;
+  a{
+    color: #18CD0C  ;
+    text-decoration: none;
+    font-weight: 500;
+    &:hover{
+      color: #15b10aa6 ;
+    }
+  }
+`;
+const Developer = styled.p`
+  font-size: 1.2rem;
+`;
+export default function Header() {
+  return (
+    <Head>
+      <h1>Book App</h1>
+      <span>
+        <Developer>
+          Developed By{" "}
+          <a href="https://github.com/mahdl-karami" target="_blank" rel="noreferrer">
+            Mahdl-Karami
+          </a>
+        </Developer>
+      </span>
+    </Head>
+  );
+}
